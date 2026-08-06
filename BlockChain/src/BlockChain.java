cbc1350925d89484b5ea1b3abc927eaa1d2b6ee3
Marioisthebekst <80685718+Marioisthebekst@users.noreply.github.com>
2026-08-06 18:47:17 +0300

    Update BlockChain.java

--- BlockChain/src/BlockChain.java
+++ BlockChain/src/BlockChain.java
@@ -1,11 +1,14 @@
+import java.security.PublicKey;
 import java.util.ArrayList;
 
 public class BlockChain {
     private ArrayList<Block> chain;
     private int difficulty;
+    private AccountState accountState;
     public BlockChain() {
         this.chain = new ArrayList<Block>();
         this.difficulty = 6;
+        this.accountState = new AccountState();
         addGenesisBlock();
     }
 
@@ -18,16 +21,38 @@ public class BlockChain {
     }
 
     public void addBlock(String data) {
-        String previousHash = getLatestBlock().hash;
+        String previousHash = getLatestBlock().getHash();
         Block newBlock = new Block(data, previousHash);
         newBlock.mineBlock(this.difficulty);
         this.chain.add(newBlock);
     }
 
+    public boolean processTransaction(Transaction tx) {
+        if (!tx.verifySignature()) {
+            System.out.println("Transaction Signature failed to verify");
+            return false;
+        }
+        if (!accountState.hasEnoughBalance(tx.sender, tx.amount)) {
+            System.out.println("Not enough balance for transaction.");
+            return false;
+        }
+        accountState.applyTransaction(tx);
+        return true;
+    }
+
+    public float getBalance(PublicKey address) {
+        return accountState.getBalance(address);
+    }
+
+    public void giveInitialCoins(PublicKey recipient, float amount) {
+        Transaction initialTx = new Transaction(null, recipient, amount);
+        this.accountState.applyTransaction(initialTx);
+    }
+
     public boolean isChainValid() {
         for (int i = 1; i < this.chain.size(); i++) {
-            if(!this.chain.get(i).calculateHash().equals(this.chain.get(i).hash) ||
-            !this.chain.get(i).previousHash.equals(this.chain.get(i - 1).hash)) {
+            if(!this.chain.get(i).calculateHash().equals(this.chain.get(i).getHash()) ||
+            !this.chain.get(i).getPreviousHash().equals(this.chain.get(i - 1).getHash())) {
                 return false;
             }
         }
